3ca257c50c155f2697ce315732a5a3f909857e49
Benjamin Soltoff <ben@bensoltoff.com>
2022-07-11 08:51:11 -0500

    Update to use new rtweet version with API v2

--- 01-twitter-solution.R
+++ 01-twitter-solution.R
@@ -1,26 +1,38 @@
 library(tidyverse)
 library(rtweet)
 
-# store authentication information
-# use pre-generated tokens just for this class
-token <- create_token(
-  app = "CFSS test 2",
-  consumer_key = getOption("twitter_api_key"),
-  consumer_secret = getOption("twitter_api_secret_key"),
-  access_token = getOption("twitter_access_token"),
-  access_secret = getOption("twitter_access_token_secret")
-)
+# interactively enter bearer token provided in class
+auth <- rtweet_app()
+
+# save and use as the default token
+auth_save(auth = auth, name = "cfss")
+auth_as("cfss")
 
 # Find the 1000 most recent tweets by
 ## Katy Perry (https://twitter.com/katyperry)
 ## Kim Kardashian West (https://twitter.com/KimKardashian)
 ## Rihanna (https://twitter.com/rihanna)
-popstars <- get_timelines(
-  user = c("katyperry", "KimKardashian", "rihanna"),
+katy_perry <- get_timeline(
+  user = "katyperry",
   n = 1000
 )
 
-# Visualize their tweet frequency by week
-popstars %>%
+kim_kardashian <- get_timeline(
+  user = "KimKardashian",
+  n = 1000
+)
+
+rihanna <- get_timeline(
+  user = "rihanna",
+  n = 1000
+)
+
+# combine, group by character, and plot weekly tweet frequency
+bind_rows(
+  `Katy Perry` = katy_perry %>% select(created_at),
+  `Kim Kardashian` = kim_kardashian %>% select(created_at),
+  `Rihanna` = rihanna %>% select(created_at),
+  .id = "screen_name"
+) %>%
   group_by(screen_name) %>%
-  ts_plot(by = "week")
+  ts_plot(by = "months")
--- 01-twitter.R
+++ 01-twitter.R
@@ -1,15 +1,13 @@
 library(tidyverse)
+library(tidyverse)
 library(rtweet)
 
-# Authenticate access to the Twitter API using the token
-# provided for class today
-token <- create_token(
-  app = "CFSS test 2",
-  consumer_key = getOption("twitter_api_key"),
-  consumer_secret = getOption("twitter_api_secret_key"),
-  access_token = getOption("twitter_access_token"),
-  access_secret = getOption("twitter_access_token_secret")
-)
+# interactively enter bearer token provided in class
+auth <- rtweet_app()
+
+# use as the default token
+auth_save(auth = auth, name = "cfss")
+auth_as("cfss")
 
 # Find the 1000 most recent tweets by
 ## Katy Perry (https://twitter.com/katyperry)
