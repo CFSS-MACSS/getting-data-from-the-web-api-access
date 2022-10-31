81d1f64b8202eb05be24bcd53bf4fa0d4f0a649c
Benjamin Soltoff <ben@bensoltoff.com>
2022-10-31 10:59:49 -0400

    More ambitious but cannot implement faceting with ts_plot()

--- 01-twitter-solution.R
+++ 01-twitter-solution.R
@@ -9,30 +9,42 @@ auth_save(auth = auth, name = "cis")
 auth_as("cis")
 
 # Find the 1000 most recent tweets by
-## Katy Perry (https://twitter.com/katyperry)
-## Kim Kardashian (https://twitter.com/KimKardashian)
-## Rihanna (https://twitter.com/rihanna)
-katy_perry <- get_timeline(
-  user = "katyperry",
-  n = 1000
+# Sesame Street
+# - Count Von Count (https://twitter.com/CountVonCount)
+# - Big Bird (https://twitter.com/BigBird)
+# - Elmo (https://twitter.com/elmo)
+# - Cookie Monster (https://twitter.com/MeCookieMonster)
+
+# The Muppets
+# - Kermit the Frog (https://twitter.com/KermitTheFrog)
+# - Miss Piggy (https://twitter.com/MissPiggy)
+# - Statler and Waldorf (https://twitter.com/StatlerWaldorf)
+# - Fozzie Bear (https://twitter.com/FozzieBear)
+
+# define screennames with source info
+characters <- tribble(
+  ~program, ~screen_name,
+  "Sesame Street", "CountVonCount",
+  "Sesame Street", "BigBird",
+  "Sesame Street", "Elmo",
+  "Sesame Street", "MeCookieMonster",
+  "The Muppets", "KermitTheFrog",
+  "The Muppets", "MissPiggy",
+  "The Muppets", "StatlerWaldorf",
+  "The Muppets", "FozzieBear"
 )
 
-kim_kardashian <- get_timeline(
-  user = "KimKardashian",
-  n = 1000
-)
+# get timelines
+timelines <- get_timeline(characters$screen_name, n = 1000)
 
-rihanna <- get_timeline(
-  user = "rihanna",
-  n = 1000
-)
+# combine user data to recover screennames
+users_timelines <- users_data(timelines)
 
-# combine, group by character, and plot weekly tweet frequency
-bind_rows(
-  `Katy Perry` = katy_perry %>% select(created_at),
-  `Kim Kardashian` = kim_kardashian %>% select(created_at),
-  `Rihanna` = rihanna %>% select(created_at),
-  .id = "screen_name"
-) %>%
-  group_by(screen_name) %>%
+# combine timelines with user data, group by character, and plot weekly tweet frequency
+users_timelines %>%
+  select(name, screen_name) %>%
+  bind_cols(timelines) %>%
+  # connect screen_name to program of origin
+  left_join(characters) %>%
+  group_by(screen_name, program) %>%
   ts_plot(by = "months")
